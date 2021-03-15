3b6a275977e1ba9f43947a25ef23a9b7d591b48f
SILVIADANIELARINCON <developer.silviadaniela@gmail.com>
2021-03-15 18:17:56 -0500

    Commit No 2 Eje6

--- Ej6.rb
+++ Ej6.rb
@@ -1,12 +1,11 @@
 puts 'Welcome'
-puts 'Algorithm for determining the area and volume of a cylinder'
-puts 'Enter the value of the radius'
-radius=gets.to_i
-puts 'Enter the value of the height'
-height=gets.to_i
+puts 'This algorithm that allows you to read a value and say if it is even or odd'
+puts 'Enter any values'
+a=gets.to_i
 
-print 'the  volume value is '
-puts (radius**2 * height * 3.1416) 
 
-print 'the area value is '
-puts (2 * (radius * height * 3.1416)) 
+if a%2 == 0 
+  puts 'The number is even'
+else 
+  puts 'The number is odd'
+end
