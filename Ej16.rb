fc7a2b29bd1c8fba84f1da4d00d05ce6e1efc9a1
SILVIADANIELARINCON <developer.silviadaniela@gmail.com>
2021-03-29 22:27:17 -0500

    Commit No3 Ej16

--- Ej16.rb
+++ Ej16.rb
@@ -9,21 +9,22 @@ leftover_50 = n-(bill50*50_000)
 bill20=leftover_50/20_000
 leftover_20 = leftover_50-(bill20*20_000)
 
-bill10=vueltos_20/10_000
+bill10=leftover_20/10_000
 leftover_10 = leftover_20-(bill10*10_000)
 
-bill5=vueltos_10/5_000
-leftover_5 = leftover_10-(bill5l*5_000)
+bill5=leftover_10/5_000
+leftover_5 = leftover_10-(bill5*5_000)
 
-bill2=vueltos_5/2_000
+bill2=leftover_5/2_000
 leftover_2 = leftover_5-(bill2*2_000)
 
-bill1=vueltos_2/1_000
+bill1=leftover_2/1_000
 leftover_1 = leftover_2-(bill1*1_000)
 
 leftover_0 = leftover_1
 
-puts "The entered value $ #{n} equates to the following banknotes #{bcincuentamil} out of fifty thousand, 
-#{bveintemil} out of twenty thousand, #{bdiezmil} out of ten thousand, #{bcincomil} out of five thousand, #{bdosmil} out of two thousand, #{bmil} out of one thousand "
+puts "The entered value $ #{n} equates to the following banknotes #{bill50} out of fifty thousand, 
+#{bill20} out of twenty thousand, #{bill10} out of ten thousand, #{bill5} out of five thousand, 
+#{bill2} out of two thousand, #{bill1} out of one thousand 
+This bank only has banknotes $#{leftover_0} units were not recalled"
 
-"This bank only has banknotes $#{vueltos_0} units were not recalled"
